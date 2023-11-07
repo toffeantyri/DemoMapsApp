ad1e9073b427326f95c067b44a20cfde50474fd6
toffeantyri <toffeantyri@gmail.com>
2023-11-07 18:44:24 +0500

    remove Permission.kt add check permission to main activity with register callback before onStarted

--- app/src/main/java/ru/toffeantyri/demomapsapp/MainActivity.kt
+++ app/src/main/java/ru/toffeantyri/demomapsapp/MainActivity.kt
@@ -1,15 +1,17 @@
 package ru.toffeantyri.demomapsapp
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.yandex.mapkit.Animation
-import com.yandex.mapkit.MapKit
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
 import ru.toffeantyri.demomapsapp.databinding.ActivityMainBinding
-import ru.toffeantyri.demomapsapp.utils.checkPermissionSingle
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,36 +30,74 @@ class MainActivity : AppCompatActivity() {
                 ), Animation(Animation.Type.SMOOTH, 5f), null
             )
         }
+        initLocationCoarsePermission()
+        initMapView()
+    }
+
+    private fun initMapView() {
+        val mapKit = MapKitFactory.getInstance()
+        val trafficLayer = mapKit.createTrafficLayer(binding.mapview.mapWindow)
+        val userLocation = mapKit.createUserLocationLayer(binding.mapview.mapWindow)
+        userLocation.isVisible = true
+        binding.ovalButton.setOnClickListener {
+            trafficLayer.isTrafficVisible = !trafficLayer.isTrafficVisible
+        }
+    }
+
+    private val locationCoarsePermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            initLocationFinePermission()
+        } else {
+            Toast.makeText(this, getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initLocationCoarsePermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_PHONE_STATE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            initLocationFinePermission()
+        } else {
+            locationCoarsePermission.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    private val locationFinePermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(this, getString(R.string.permission_granted), Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initLocationFinePermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_PHONE_STATE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(this, getString(R.string.permission_granted), Toast.LENGTH_SHORT).show()
+        } else {
+            locationFinePermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     override fun onStart() {
         super.onStart()
         MapKitFactory.getInstance().onStart()
         binding.mapview.onStart()
-        val mapKit = MapKitFactory.getInstance()
-        initMapView(mapKit)
-        checkPermissionSingle(Manifest.permission.ACCESS_FINE_LOCATION) {
-            checkPermissionSingle(Manifest.permission.ACCESS_COARSE_LOCATION) {
-                val userLocation = mapKit.createUserLocationLayer(binding.mapview.mapWindow)
-                userLocation.isVisible = true
-            }
-        }
     }
 
     override fun onStop() {
         binding.mapview.onStop()
         MapKitFactory.getInstance().onStop()
         super.onStop()
-    }
-
-
-    private fun initMapView(mapKit: MapKit) {
-        val trafficLayer = mapKit.createTrafficLayer(binding.mapview.mapWindow)
-        binding.ovalButton.setOnClickListener {
-            trafficLayer.isTrafficVisible = !trafficLayer.isTrafficVisible
-        }
-
-
     }
 
 }
